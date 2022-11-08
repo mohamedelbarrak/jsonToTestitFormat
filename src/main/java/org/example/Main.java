f1314d3c666d7b48dcbc7823bb9e1a32750165ec
Mohamed Elbarrak <mohamedelbarrak@gmail.com>
2022-11-08 13:24:41 +0100

    com1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
-public class Main1 {
-    public void main1(String[] args) {
-        System.out.println("Main1 match");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("your file must be like this:");
+        System.out.println("{\n...\n}");
         try
         {
             // Le fichier d'entrée
@@ -18,20 +20,67 @@ public class Main1 {
             BufferedReader br = new BufferedReader(fr);
             StringBuffer sb = new StringBuffer();
             String line;
+            Boolean f = false;
             while((line = br.readLine()) != null)
             {
-
+                line = AddSlach(line);// \
+                line = AddQuotationMarks(line);// "
+                if(f==true)
+                line = AddPlus(line);// +
+                f = true;
                 // ajoute la ligne au buffer
                 sb.append(line);
                 sb.append("\n");
             }
             fr.close();
-            System.out.println("Testit format: ");
+            System.out.println("Testit format of your file: ");
             System.out.println(sb.toString());
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
+    }
+
+    private static String AddPlus(String line) {
+        StringBuffer str = new StringBuffer(line);
+        // ajouter le caractère au début de la chaîne
+        str.insert(0,'+');
+        //System.out.println("AddQuotationMarks line: "+str);
+        String lineDone = String.valueOf(str);
+        return lineDone;
+    }
+
+    private static String AddSlach(String line) {
+        StringBuffer str = new StringBuffer(line);
+        List<Integer> list1 = new ArrayList<Integer>();
+        int j=0;
+        for(int i=0; i<line.length(); i++){
+            if(line.charAt(i) == '\"') {
+                list1.add(i+j);
+                j++;
+            }
+        }
+
+        for (int z=0; z < list1.size() ; z++) {
+            char a = '\\';
+            int aa = list1.get(z);
+            str.insert(aa, '\\');
+        }
+        // ajouter le caractère L au milieu de la chaîne
+        //str.insert(5,'\\');
+        String lineDone = String.valueOf(str);
+        return lineDone;
+    }
+
+    private static String AddQuotationMarks(String line) {
+        StringBuffer str = new StringBuffer(line);
+        // ajouter le caractère au début de la chaîne
+        str.insert(0,'"');
+        // ajouter le caractère à la fin de la chaîne
+        str.append('"');
+        //System.out.println("AddQuotationMarks line: "+str);
+        String lineDone = String.valueOf(str);
+        return lineDone;
     }
 }
